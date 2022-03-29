46228a2e013a48b6c0a9b7b396561943a26d00c7
boscha1 <bosch.anthony7@gmail.com>
2022-03-28 22:57:04 -0400

    implemented find all method

--- src/main/java/org/antiqueauto/services/service/CustomerService.java
+++ src/main/java/org/antiqueauto/services/service/CustomerService.java
@@ -3,24 +3,40 @@ package org.antiqueauto.services.service;
 import org.antiqueauto.services.data.SampleData;
 import org.antiqueauto.services.domain.Customer;
 import org.antiqueauto.services.exception.customer.CustomerNotFoundException;
+import org.antiqueauto.services.repository.car.CarDAO;
+import org.antiqueauto.services.repository.customer.CustomerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
 
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Autowired
+    private CarDAO carDAO;
+
+
     public Customer create(Customer customer) {
         try {
-            SampleData.customers.add(customer);
-            return customer;
+            // SampleData.customers.add(customer);
+            return customerDAO.save(customer);
         } catch (Exception e) {
             throw new IllegalStateException(e.getMessage());
         }
     }
 
     public List<Customer> findAll() {
-        return SampleData.customers;
+        // return SampleData.customers;
+        List<Customer> customers = customerDAO.findAll();
+        customers.stream()
+                .peek(customer -> customer.setCars(carDAO.findByCustomerId(customer.getCustomerId())))
+                .collect(Collectors.toList());
+        return customers;
     }
 
     public Customer findById(Integer customerId) {
