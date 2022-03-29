3107671c7c6a711efea8f4f40d404d80c50c94c1
boscha1 <bosch.anthony7@gmail.com>
2022-03-29 16:09:09 -0400

    added id

--- src/main/java/org/antiqueauto/services/exception/car/CarNotFoundException.java
+++ src/main/java/org/antiqueauto/services/exception/car/CarNotFoundException.java
@@ -1,16 +1,23 @@
 package org.antiqueauto.services.exception.car;
 
 public class CarNotFoundException extends RuntimeException {
-    private final String code;
+    private String code;
+    private Integer id;
     public CarNotFoundException(String code) {
         super(String.format("Car %s not found", code));
         this.code = code;
+    }
+
+    public CarNotFoundException(Integer id) {
+        super(String.format("Car %d not found", id));
+        this.id = id;
     }
 
     @Override
     public String toString() {
         return "CarNotFoundException{" +
                 "code='" + code + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
