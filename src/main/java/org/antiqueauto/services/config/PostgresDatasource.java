323079f59d8f2ca7a2b28af2d8cac5a78b262ae6
boscha1 <bosch.anthony7@gmail.com>
2022-06-01 09:18:15 -0400

    fixed resources for database connection

--- src/main/java/org/antiqueauto/services/config/PostgresDatasource.java
+++ src/main/java/org/antiqueauto/services/config/PostgresDatasource.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class PostgresDatasource {
 
@@ -16,5 +17,10 @@ public class PostgresDatasource {
                 .create()
                 .type(HikariDataSource.class)
                 .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource) {
+        return new JdbcTemplate(hikariDataSource);
     }
 }
